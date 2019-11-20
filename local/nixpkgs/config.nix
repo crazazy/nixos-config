ebe80482e370f342903a0c92ce4fdc50d64d9721
Erik Oosting <erik99.oosting@gmail.com>
2019-11-20 21:57:56 +0100

    formatted local config

--- local/nixpkgs/config.nix
+++ local/nixpkgs/config.nix
@@ -1,58 +1,58 @@
 {
-	allowUnfree = true;
-	packageOverrides = defaultPackages: with defaultPackages.pkgs; {
-		python-pacman-packages = python-packages: with python-packages; [
-			pygame
-			ConfigArgParse
-			virtualenvwrapper
-		];
-		python-mod5pacman = python3.withPackages python-pacman-packages;
+  allowUnfree = true;
+  packageOverrides = defaultPackages: with defaultPackages.pkgs; {
+    python-pacman-packages = python-packages: with python-packages; [
+      pygame
+      ConfigArgParse
+      virtualenvwrapper
+    ];
+    python-mod5pacman = python3.withPackages python-pacman-packages;
 
-		rEnv = defaultPackages.rWrapper.override {
-			packages = with rPackages; [
-				foreign
-				ggplot2
-				BoardGames
-				shiny
-				plumber
-				rmarkdown
-			];
-		};
+    rEnv = defaultPackages.rWrapper.override {
+      packages = with rPackages; [
+        foreign
+        ggplot2
+        BoardGames
+        shiny
+        plumber
+        rmarkdown
+      ];
+    };
 
-		all-env = buildEnv {
-			name = "all-env";
-			paths = [ user-env dev-env games-env];
-		};
-		user-env = buildEnv {
-			name = "user-env";
-			paths = [
-				firefox
-				gparted
-				xclip
-				xbanish
-				vlc
-				ffmpeg
-				libreoffice
-			];
-		};
-		dev-env = buildEnv {
-			name = "dev-env";
-			paths = [
-				git
-				python-mod5pacman
-				nodejs
-				rEnv
-				jetbrains.pycharm-community
-				jetbrains.idea-community
-			];
-		};
-		games-env = buildEnv {
-			name = "games-env";
-			paths = [
-				minetest
-				stuntrally
-				];
-		};
-	};
+    all-env = buildEnv {
+      name = "all-env";
+      paths = [ user-env dev-env games-env];
+    };
+    user-env = buildEnv {
+      name = "user-env";
+      paths = [
+        firefox
+        gparted
+        xclip
+        xbanish
+        vlc
+        ffmpeg
+        libreoffice
+      ];
+    };
+    dev-env = buildEnv {
+      name = "dev-env";
+      paths = [
+        git
+        python-mod5pacman
+        nodejs
+        rEnv
+        jetbrains.pycharm-community
+        jetbrains.idea-community
+      ];
+    };
+    games-env = buildEnv {
+      name = "games-env";
+      paths = [
+        minetest
+        stuntrally
+        ];
+    };
+  };
 }
 
