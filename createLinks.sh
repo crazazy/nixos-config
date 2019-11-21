7be055fc42c1d8d35a2f171af3916a46c2987690
Erik Oosting <erik99.oosting@gmail.com>
2019-11-21 11:44:09 +0100

    made createLinks location-independent

--- createLinks.sh
+++ createLinks.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 CONFIG=$HOME/.config
-ln -s /etc/nixos/local/nixpkgs $CONFIG
-ln -s /etc/nixos/local/openbox $CONFIG
-ln -s /etc/nixos/local/ulauncher $CONFIG
-ln -s /etc/nixos/local/ulauncher $CONFIG
+echo $(dirname $0)
+CURRENT=$(dirname $0| sed -e s@\\.@$(pwd)@g)
+ln -s $CURRENT/local/nixpkgs $CONFIG
+ln -s $CURRENT/local/openbox $CONFIG
+ln -s $CURRENT/local/ulauncher $CONFIG
+ln -s $CURRENT/local/xfce4 $CONFIG
