04d171d9e1f72411f367653f984ee250917ea26e
Erik Oosting <erik99.oosting@gmail.com>
2019-11-26 16:34:58 +0100

    added coc plugins

--- vim-configuration.nix
+++ vim-configuration.nix
@@ -3,7 +3,23 @@ let
   myVim = pkgs.vim_configurable.customize {
     name = "vi";
     vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
-      start = [coc-nvim vim-polyglot];
+      start = [
+        vim-polyglot
+        coc-nvim
+        coc-html
+        coc-css
+        coc-emmet
+        coc-java
+        coc-go
+        coc-json
+        coc-prettier
+        coc-python
+        coc-r-lsp
+        coc-tslint
+        coc-tsserver
+        coc-vetur
+        coc-yaml
+      ];
     };
   };
 in
