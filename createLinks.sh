9f912de64fa6567fdcc548c8950a9a9aaff06fca
Erik Oosting <erik99.oosting@gmail.com>
2019-12-03 19:33:02 +0100

    updated vim config

--- createLinks.sh
+++ createLinks.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 CONFIG=$HOME/.config
 CURRENT=$(dirname $0| sed -e s@\\.@$(pwd)@g)
-ln -s $CURRENT/local/nixpkgs $CONFIG
-ln -s $CURRENT/local/openbox $CONFIG
-ln -s $CURRENT/local/ulauncher $CONFIG
-ln -s $CURRENT/local/xfce4 $CONFIG
+for i in $(ls $CURRENT/local); do
+	ln -s $CURRENT/local/$i $CONFIG
+done
