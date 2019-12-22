59b4edfa585e69fa63907d21f06f96099eadf85d
Erik Oosting <erik99.oosting@gmail.com>
2019-12-22 18:09:06 +0100

    added networks to lemonbar config

--- local/openbox/lemonbar.sh
+++ local/openbox/lemonbar.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-Clock() {
-        DATETIME=$(date "+%a %b %d, %H:%M")
+Wifi() {
+    local WIFI=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d: -f2)
+    if [ -n WIFI ]; then
+        echo -n "Connected on: $WIFI"
+    else
+        echo -n "Not connected to WiFi"
+    fi
+}
 
-        echo -n "$DATETIME"
+Clock() {
+    local DATETIME=$(date "+%a %b %d, %H:%M")
+    echo -n "$DATETIME"
 }
 
 Battery() {
-    BATPERC=$(cat /sys/class/power_supply/BAT0/capacity)
+    local BATPERC=$(cat /sys/class/power_supply/BAT0/capacity)
     echo -n "$BATPERC %"
 }
 
 # Print the clock
 
 while true; do
-    echo "%{c}%{F#FFFF00}%{B#0000FF} Battery: $(Battery) : $(Clock) %{F-}%{B-}"
+    echo "%{c}%{F#FFFF00}%{B#0000FF}$(Wifi) : Battery: $(Battery) : $(Clock) %{F-}%{B-}"
         sleep 20
 done
