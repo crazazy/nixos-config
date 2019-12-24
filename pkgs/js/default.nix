efd96f6dd12f007c0b17dbc4861001dd21f75990
Erik Oosting <erik99.oosting@gmail.com>
2019-12-24 20:40:32 +0100

    added webpack cli

--- pkgs/js/default.nix
+++ pkgs/js/default.nix
@@ -6,6 +6,7 @@
   pnpm = (import ./pnpm { inherit pkgs; }).package;
   preact-cli = (import ./preact-cli {inherit pkgs; }).package;
   npe = (import ./npe { inherit pkgs; }).package;
+  webpack = (import ./webpack-cli { inherit pkgs; }).package;
 
   nix-gen-node-tools = pkgs.callPackage ./gen-node-env { };
 }
