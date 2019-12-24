105f57a28209e18c3c5a72a66ab74fb1bfde994a
Erik Oosting <erik99.oosting@gmail.com>
2019-12-24 15:32:27 +0100

    pkgs: added js.pnpm

--- pkgs/js/default.nix
+++ pkgs/js/default.nix
@@ -3,6 +3,7 @@
   rollup = (import ./rollup { inherit pkgs; }).package;
   parcel = (import ./parcel-bundler { inherit pkgs; }).package;
   CRA = (import ./create-react-app { inherit pkgs; }).package;
+  pnpm = (import ./pnpm { inherit pkgs; }).package;
   preact-cli = (import ./preact-cli {inherit pkgs; }).package;
   npe = (import ./npe { inherit pkgs; }).package;
 
