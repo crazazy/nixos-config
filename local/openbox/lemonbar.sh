27aba897284498720daa7315d8a28a40e6535385
Erik Oosting <erik99.oosting@gmail.com>
2019-12-27 12:33:46 +0100

    added volume to lemonbar

--- local/openbox/lemonbar.sh
+++ local/openbox/lemonbar.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+Volume() {
+    local VOLUME=$(amixer sget Master | grep '^\s*Front' | sed -e 's/[^0-9[]*//g' | cut -d [ -f 2 | head -n 1)
+    echo -n "Volume: $VOLUME %"
+}
 
 Backlight() {
     local BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/brightness)
@@ -27,6 +31,6 @@ Battery() {
 # Print the clock
 
 while true; do
-    echo "%{c}%{F#FFFF00}%{B#0000FF}$(Wifi) : $(Backlight) : $(Battery) : $(Clock) %{F-}%{B-}"
+    echo "%{c}%{F#FFFF00}%{B#0000FF}$(Wifi) : $(Backlight) : $(Volume) : $(Battery) : $(Clock) %{F-}%{B-}"
         sleep 5
 done
