40740df5a8c91710f485f844e58903b7c8fed81f
Erik Oosting <erik99.oosting@gmail.com>
2020-02-20 18:25:44 +0100

    added classicube, package can't be installed yet due to see comment at top

---
+++ pkgs/ClassiCube/default.nix
@@ -0,0 +1,21 @@
+# TODO: right now ClassiCube tries to load it's assets into the installed directory ($out/bin)
+# we have to wait and see if this becomes configurable before we can actually install this package
+{gcc, curl, freetype, openal, libGL, SDL2, xorg, stdenv, fetchFromGitHub}:
+stdenv.mkDerivation rec {
+  name = "ClassiCube";
+  version = "1.1.2";
+  src = fetchFromGitHub {
+    repo = name;
+    owner = "UnknownShadow200";
+    rev = version;
+    sha256 = "1xir6slwrgzma9m5gmv4nw81ihz40vr57cks76sgghanablzig00";
+  };
+  preBuild = ''
+    cd ./src
+  '';
+  installPhase = ''
+    install -D ClassiCube $out/bin/ClassiCube
+    '';
+  nativeBuildInputs = [ gcc ];
+  buildInputs = [freetype openal curl libGL SDL2 xorg.libX11]; 
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,7 @@
   # standalone packages
   nix-gen-node-tools = pkgs.callPackage ./gen-node-env { inherit (pkgs.nodePackages) node2nix; };
   efm-langserver = pkgs.callPackage ./efm-langserver {};
+  ClassiCube = pkgs.callPackage ./ClassiCube {};
 
   # modules
   modules = import ../modules;
