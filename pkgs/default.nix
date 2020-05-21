9e67afa45d5425a10ad5e838b9706b73206539eb
Erik Oosting <erik99.oosting@gmail.com>
2020-05-21 20:01:29 +0200

    added niv again

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ 
+  pkgs ? import <nixpkgs> {},
+  sources ? import ../nix/sources.nix }:
 {
   # package sets
   js = import ./js { inherit pkgs; };
 
   # standalone packages
   nix-gen-node-tools = pkgs.callPackage ./gen-node-env { inherit (pkgs.nodePackages) node2nix; };
-  efm-langserver = pkgs.callPackage ./efm-langserver {};
-  ClassiCube = pkgs.callPackage ./ClassiCube {};
+  efm-langserver = pkgs.callPackage ./efm-langserver { inherit sources; };
+  ClassiCube = pkgs.callPackage ./ClassiCube { inherit sources; };
 
   # modules
   modules = import ../modules;
--- pkgs/efm-langserver/default.nix
+++ pkgs/efm-langserver/default.nix
@@ -1,14 +1,9 @@
-{ buildGoModule, fetchFromGitHub, lib}:
+{ buildGoModule, fetchFromGitHub, lib, sources }:
 buildGoModule rec {
   name = "efm-langserver";
   version = "0.0.14";
-  src = fetchFromGitHub {
-    owner = "mattn";
-    repo = "efm-langserver";
-    rev = "v${version}";
-    sha256 = "0gswjz9m68xl8r9f11bqp71lvb0b8j0j7vy0nkiwf4rmsyg0mjnb";
-  };
-  modSha256 = "0mi0rhfxbb06jjc87jl07kg9dgjysracc0ndjwmz9vic5jrjvydz";
+  src = sources.efm-langserver;
+  vendorSha256 = "0cc55yswyls34s6ryc9r3vlxc90dq5xgm4fz6aynjhw7f48scf46";
   subPackages = ["."];
 
   meta = with lib; {
