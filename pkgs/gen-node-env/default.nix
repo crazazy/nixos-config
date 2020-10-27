9098b79fd78a0cc51e3dc2459bf594a9f6d25b89
Erik Oosting <erik99.oosting@gmail.com>
2020-10-27 20:17:01 +0100

    removed extra programs from nix-gen-node-tools

--- pkgs/gen-node-env/default.nix
+++ pkgs/gen-node-env/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchzip, jq, curl, node2nix, sources }:
+{ stdenv, fetchzip, jq, curl, node2nix, makeWrapper, sources }:
+let
+  binPath = stdenv.lib.makeBinPath [ jq curl node2nix ];
+in
 stdenv.mkDerivation {
   pname = "nix-gen-node-tools";
   version = "1.0.2";
 
   src = fetchzip { inherit (sources.nix-gen-node-tools) url sha256; };
 
-  buildInputs = [ jq curl node2nix ];
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
-    install -m755 -D genNodeNix "$out/bin/nix-gen-node-tools"
-    install -m755 -D ${jq}/bin/jq "$out/bin/jq"
-    install -m755 -D ${curl}/bin/curl "$out/bin/curl"
-    install -m755 -D ${node2nix}/bin/node2nix "$out/bin/node2nix"
-  '';
+    makeWrapper genNodeNix $out/bin/nix-gen-node-tools --set PATH ${binPath}
+ '';
 
   meta = with stdenv.lib; {
     description = "Generates nix expressions for node.js CLI tools written in pure JS";
