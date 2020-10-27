c772d2e78158585991c826b71c17c1f3405ea062
Erik Oosting <erik99.oosting@gmail.com>
2020-10-27 19:56:08 +0100

    removed niv source wonk

--- pkgs/emacs/default.nix
+++ pkgs/emacs/default.nix
@@ -1,5 +1,5 @@
 { pkgs, sources, fetchzip }:
 let
-  emacsSrc = (fetchzip { inherit (sources.emacs) url sha256; }).outPath;
+  emacsSrc = (fetchzip { inherit (sources.emacs) url sha256; });
 in
   import emacsSrc { inherit pkgs; }
