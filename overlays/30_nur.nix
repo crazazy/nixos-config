2d61b0047b378eccbe410d7f70cd335f62bab166
Erik Oosting <erik99.oosting@gmail.com>
2021-02-03 19:57:06 +0100

    fixed bug with general nur overlay

--- overlays/30_nur.nix
+++ overlays/30_nur.nix
@@ -2,7 +2,7 @@ self: super: let
    inherit (builtins) fetchTarball;
    nur = super.callNixPackage (
       { pkgs }:
-      import lib.srcs.user-pkgs { inherit pkgs; nurpkgs = pkgs; }
+      import super.lib.srcs.user-pkgs { inherit pkgs; nurpkgs = pkgs; }
       ) {};
    in
    { inherit (nur) repos repo-sources; }
