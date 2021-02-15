c00832744f7d985d613a9f5a92b9d62dd24791b3
Erik Oosting <erik99.oosting@gmail.com>
2021-02-15 11:28:00 +0100

    make userspace more strict

--- overlays/90_userspace.nix
+++ overlays/90_userspace.nix
@@ -2,4 +2,5 @@
 self: super: {
    callNixPackage = throw "cannot use callNixPackage here, since it would add extra packages beyond what is defined in the overlays";
    pkgs = self;
+   lib = builtins.removeAttrs super.lib ["srcs" "overlays" "pkgsrc"];
 }
