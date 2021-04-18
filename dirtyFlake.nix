3146ee59520e2a36a2760d389b7fcb92c6bd7285
Erik Oosting <erik99.oosting@gmail.com>
2021-04-18 22:22:12 +0200

    added new registries to nixos

--- dirtyFlake.nix
+++ dirtyFlake.nix
@@ -8,7 +8,7 @@
     inherit (v) url;
     flake = v.flake or false;
   }) packageSources;
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     let
       inherit (flake-utils.lib) eachSystem flattenTree;
       mapAttrs = f: attrs: builtins.listToAttrs (map (k: { name = k; value = f k attrs.${k}; }) (builtins.attrNames attrs));
@@ -25,6 +25,10 @@
     {
       nixosConfigurations.RACEMONSTER = mkSystem [ 
         ./configuration.nix
+        ({config, lib, ...}: {
+          nix.registry = lib.mkIf config.nix-unstable.enable (
+            lib.mapAttrs (k: v: { flake = v; }) inputs);
+          })
       ];
       nixosModules = mapAttrs (k: v: import v)
         { inherit (import ./modules) feh-bg-module home-manager; };
