8404e96a741cf9578ba39a2f7a72b73c74e1da67
Erik Oosting <erik99.oosting@gmail.com>
2021-12-08 13:20:37 +0100

    update trufflesqueak

--- pkgs/truffleSqueak/default.nix
+++ pkgs/truffleSqueak/default.nix
@@ -1,8 +1,8 @@
 { graalvm11-ce, fetchurl, SDL2, autoPatchelfHook }:
-graalvm11-ce.overrideAttrs (old: {
+graalvm17-ce.overrideAttrs (old: {
   srcs = old.srcs ++ [
     (fetchurl {
-      url = "https://github.com/hpi-swa/trufflesqueak/releases/download/21.2.0/trufflesqueak-installable-svm-java11-linux-amd64-21.2.0.jar";
+      url = "https://github.com/hpi-swa/trufflesqueak/releases/download/21.3.0/trufflesqueak-installable-java11-linux-amd64-21.3.0.jar";
       sha256 = "0znk3mm356wck6anqvgfaw154sskqgkvlyhxclsshh70vg4pq2s1";
     })];
   autoPatchelfIgnoreMissingDeps = true;
