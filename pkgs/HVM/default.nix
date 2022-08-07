544960bb788051bcdb2f370019ecb136e0aec815
Erik Oosting <erik99.oosting@gmail.com>
2022-08-07 09:18:54 +0200

    forgot semicolor

--- pkgs/HVM/default.nix
+++ pkgs/HVM/default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage rec {
   src = fetchzip { inherit (HVM) url sha256; };
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
-  }
+  };
   doCheck = false; # I'm sure it'll work out fiine
   meta = {
     description = "High-level virtual machine. Evaluates based on a fancy version of term rewriting, but in parralel";
