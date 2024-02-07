698fd98f050f91db44f8c3dcfe091c025a17b762
Erik Oosting <erik99.oosting@gmail.com>
2024-02-07 17:13:41 +0100

    I forgor 💀

--- pkgs/wasmfxtime/default.nix
+++ pkgs/wasmfxtime/default.nix
@@ -3,5 +3,5 @@ wasmtime.overrideAttrs (old: {
   inherit (nvsrcs.wasmfxtime) src version;
   meta = old.meta // {
     description = "wasmtime WebAssembly runner with support for delmited continuations";
-  }
+  };
 })
