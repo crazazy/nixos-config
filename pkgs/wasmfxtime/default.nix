bc74ce20a50aa95b64367c56a04c636974a79941
Erik Oosting <erik99.oosting@gmail.com>
2024-02-07 17:55:36 +0100

    fuxking cargohash overriding my lockfile

--- pkgs/wasmfxtime/default.nix
+++ pkgs/wasmfxtime/default.nix
@@ -1,6 +1,7 @@
 { nvsrcs, wasmtime }:
 wasmtime.overrideAttrs (old: {
   inherit (nvsrcs.wasmfxtime) src version;
+  cargoHash = null;
   cargoLock = nvsrcs.wasmfxtime.cargoLock."./Cargo.lock".lockFile;
   meta = old.meta // {
     description = "wasmtime WebAssembly runner with support for delmited continuations";
